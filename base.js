1107617f7247fe8201bfb71ba158e30155ba7f8b
jshin8 <joshshin8@gmail.com>
2015-10-13 02:26:28 -0700

    edits to all files, basic functionality

--- base.js
+++ base.js
@@ -1,7 +1,22 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
-
-  // code in here
+$(document).ready(function() {
+    getPost();
+  
 
 });
+
+//function to grab input from form
+function getPost() {
+$('.btn').click(function() {
+    	event.preventDefault();	
+      	postPost($('#post').val());
+	});
+}
+//function to add input and timestamp to the DOM and then clear typebox
+function postPost(content) {
+	$('#postlist').append('<p>' + content + '</p>' + '@' + new Date($.now()));
+	$('#post').val('');
+}
+
+
